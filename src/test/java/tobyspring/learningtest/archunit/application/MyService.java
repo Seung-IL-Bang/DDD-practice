39c5f335804314a59da03d79f90dedf109fe3889
Seungil <seungilbang@khu.ac.kr>
2025-07-12 16:27:31 +0900

    Add ArchUnit dependency for architectural testing & Learning Tests for ArchUnit

---
+++ src/test/java/tobyspring/learningtest/archunit/application/MyService.java
@@ -0,0 +1,9 @@
+package tobyspring.learningtest.archunit.application;
+
+import tobyspring.learningtest.archunit.adapter.MyAdapter;
+
+public class MyService {
+    MyService2 myService2;
+
+//    MyAdapter myAdapter;
+}
---
+++ src/test/java/tobyspring/learningtest/archunit/domain/MyMember.java
@@ -0,0 +1,9 @@
+package tobyspring.learningtest.archunit.domain;
+
+import tobyspring.learningtest.archunit.adapter.MyAdapter;
+import tobyspring.learningtest.archunit.application.MyService;
+
+public class MyMember {
+//    MyService myService;
+//    MyAdapter myAdapter;
+}
